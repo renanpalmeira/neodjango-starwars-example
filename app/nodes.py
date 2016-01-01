36541e0ad9cd8fc01a9daffa9ae68890bd67db45
Renan Palmeira <renan@forleven.com>
2016-01-01 02:56:46 -0200

    Initial test a new version in Neodjango

--- app/nodes.py
+++ app/nodes.py
@@ -2,6 +2,11 @@ from __future__ import unicode_literals
 
 from neodjango.db import models
 
-class Human(models.Node):
+class Planet(models.Node):
+	class Meta:
+		display = "name"
+
+class Jedi(models.Node):
 	class Meta:
 		label = "Human"
+		display = "name"
--- app/tests.py
+++ app/tests.py
@@ -2,8 +2,30 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-from app.nodes import Human
+from app.nodes import Jedi, Planet
 
-class ArticleTestCase(TestCase):
-	def test_model(self):
-		print Human.objects.all()
+class PlanetNodeTestCase(TestCase):
+
+	def test_node_filter(self):
+		result = Jedi.objects.filter(name='Rey')
+		self.assertEqual(len(result), 1)
+
+	def test_node_all(self):
+		result = Planet.objects.all()
+		self.assertEqual(type(result), list)
+
+	def test_node_get(self):
+		result = Jedi.objects.get(name='Luke Skywalker')
+		return True
+
+	def test_node_relationships(self):
+		result = Planet.objects.get(name='Kamino').relationships()
+		terrain = result[0].terrain
+		self.assertEqual(terrain, 'ocean')
+
+	def test_multi_node(self):
+		rey = Jedi.objects.get(id=80)
+		jakku = Planet.objects.get(name='Jakku')
+		self.assertEqual(rey.name, 'Rey')
+		self.assertEqual(jakku.terrain, 'deserts')
+		
