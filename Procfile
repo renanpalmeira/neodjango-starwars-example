64857eb27e6d0d456ff31c3d5a3ca7d64f192664
Renan Palmeira <renan@forleven.com>
2016-01-08 23:41:12 -0200

    Adding tasks

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: python manage.py runserver
 redis: redis-server
-# worker: celery -A starwars worker --loglevel=info
+worker: celery -A starwars worker --loglevel=info
--- starwars/settings.py
+++ starwars/settings.py
@@ -29,6 +29,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Celery 
+# http://docs.celeryproject.org/en/latest/userguide/security.html
+
+BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json',]
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
 
 # Application definition
 
