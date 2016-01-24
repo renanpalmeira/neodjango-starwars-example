c5bb1fc0eb31f0aaf09d4cdcd8a0fc40b249ab0d
Renan Palmeira <renan@forleven.com>
2016-01-23 23:34:27 -0200

    Adding celery task in signals

--- app/signals.py
+++ app/signals.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 from neodjango.db.models.signals import pre_node_init
 
 from .nodes import Jedi
+from .tasks import add
 
 @receiver(pre_node_init, sender=Jedi)
 def initial_jedi(sender, **kwargs):
-	pass
+	add.delay(1, 2)
